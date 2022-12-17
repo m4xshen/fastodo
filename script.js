f54e828354ca424056d3e14c6f62ef52df657c36
m4xshen <m4xshen@gmail.com>
2022-12-17 16:21:47 +0800

    Add expand card to set priority & tag

--- script.js
+++ script.js
@@ -2,16 +2,26 @@ const todolist = document.getElementById('todolist');
 const template = document.getElementsByTagName('template')[0];
 
 class Todo {
-  constructor(content, tag) {
+  constructor(content, priority, tag) {
+    this.priority = priority;
     this.tag = tag;
-    this.content = content; // debug
 
     this.element = document.createElement('div');
     this.element.className = 'todo';
     this.element.innerHTML = template.innerHTML;
-    this.element.querySelector('.content').innerHTML = content;
+
+    this.content = this.element.querySelector('.content');
+    this.content.innerHTML = content;
+    this.content.setAttribute('checked', 'false');
 
     this.checkbox = this.element.querySelector('.checkbox');
+    if(priority == 'high') {
+      this.checkbox.style.borderColor = 'red';
+    }
+    else if(priority == 'mid') {
+      this.checkbox.style.borderColor = 'yellow';
+    }
+
     this.svg = this.element.querySelector('svg');
 
     addEvent(this);
@@ -22,19 +32,21 @@ const uncheckedTodo = [];
 const checkedTodo = [];
 
 function update() {
-  uncheckedTodo.forEach((todo) => {
-    todolist.append(todo.element);
-  });
   checkedTodo.forEach((todo) => {
-    todolist.append(todo.element);
+    todolist.insertBefore(todo.element, newtodo.nextSibling);
+  });
+  uncheckedTodo.forEach((todo) => {
+    todolist.insertBefore(todo.element, newtodo.nextSibling);
   });
 }
 
 function addEvent(todo) {
+  // toggle state when checkbox is clicked
   todo.checkbox.onclick = function() {
     if(todo.checkbox.getAttribute('checked') == 'false') {
       todo.checkbox.setAttribute('checked', 'true');
       todo.svg.setAttribute('checked', 'true');
+      todo.content.setAttribute('checked', 'true');
 
       uncheckedTodo.splice(uncheckedTodo.indexOf(todo), 1);
       checkedTodo.push(todo);
@@ -42,6 +54,7 @@ function addEvent(todo) {
     else {
       todo.checkbox.setAttribute('checked', 'false');
       todo.svg.setAttribute('checked', 'false');
+      todo.content.setAttribute('checked', 'false');
 
       checkedTodo.splice(checkedTodo.indexOf(todo), 1);
       uncheckedTodo.push(todo);
@@ -52,11 +65,32 @@ function addEvent(todo) {
 }
 
 const newtodo = document.getElementById('newtodo');
-newtodo.addEventListener('keydown', (event) => {
-  if(event.code=='Enter' && newtodo.value!='') {
-    uncheckedTodo.push(new Todo(newtodo.value));
-    newtodo.value = '';
+const content = document.getElementById('content');
+const tag = document.getElementById('tag');
 
-    uncheckedTodo.forEach((todo) => todolist.prepend(todo.element));
+// expand the newtodo
+content.addEventListener('focus', () => {
+  newtodo.style.maxHeight = '210px';
+})
+
+// shrink the newtodo
+document.addEventListener('click', (event) => {
+  if(event.target != content && !newtodo.contains(event.target)) {
+    newtodo.style.maxHeight = '50px';
+  }
+})
+
+// add a new todo when press Enter
+newtodo.addEventListener('keydown', (event) => {
+  if(event.code=='Enter' && content.value!='') {
+    const priority = newtodo.querySelector('input[type="radio"]:checked');
+    uncheckedTodo.push(new Todo(content.value, priority.id, tag.value));
+    content.value = '';
+    const lowPriority = document.getElementById('low');
+    lowPriority.checked = true;
+    tag.value = '';
+
+    uncheckedTodo.forEach((todo) =>
+      todolist.insertBefore(todo.element, newtodo.nextSibling));
   }
 });
