6d6b91523a7a739bc99f2b0643325a123249e13a
m4xshen <m4xshen@gmail.com>
2023-03-12 23:15:49 +0800

    Rebuild intro page with tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      sans: [
+        "Inter, sans-serif",
+      ]
+    },
+    extend: {
+      colors: {
+        yellow: "#f2c86b"
+      }
+    },
+  },
+  plugins: [],
+}
